2ec108713ea9e08be10ed16466cb9b7f0781f88c
Arib Alam <arib.alam.iitkgp@gmail.com>
2019-01-22 00:05:48 +0530

    create run.sh

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# DO NOT MODIFY THIS FILE
+
+# User's Wallpaper's directory.
+#homedir=$( getent passwd "$USER" | cut -d: -f6 )
+#dir="$homedir/Pictures/BingWallpapers/"
+fl=$(find /proc -maxdepth 2 -user "$(whoami)" -name environ -print -quit)
+for i in {1..5}
+do
+  fl=$(find /proc -maxdepth 2 -user "$(whoami)" -name environ -newer "$fl" -print -quit)
+done
+
+export DBUS_SESSION_BUS_ADDRESS
+DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS "$fl" | cut -d= -f2-)
+
+python3 python_scripts/bing_image_loader.py
