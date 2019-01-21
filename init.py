3a95175161b2822307eaaefbec4907b26612954b
Arib Alam <arib.alam.iitkgp@gmail.com>
2019-01-22 00:06:55 +0530

    create init.py

---
+++ init.py
@@ -0,0 +1,18 @@
+import os
+import getpass
+from crontab import CronTab
+
+INSTALL_DIR = os.getcwd()
+COMMENT = 'BingWallpapers'
+
+cron = CronTab(user=getpass.getuser())
+
+# Scheduling the changing of wallpapers
+job1 = cron.new(command=INSTALL_DIR + '/run.sh', comment=COMMENT)
+job1.minute.every(30)
+
+# Scheduling the download of new wallpapers
+job2 = cron.new(command='python3 ' + INSTALL_DIR + '/image_downloader.py', comment=COMMENT)
+job2.every_reboot()
+
+cron.write()
